3dc01a82896b14b8c47654fa920da888058ba729
Iman <imi.codes@gmail.com>
2026-03-31 14:14:58 +0100

    Primer commit - estructura inicial

---
+++ src/main/java/com/mycompany/tracklify/controllers/Main_viewController.java
@@ -0,0 +1,35 @@
+package com.mycompany.tracklify.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+public class Main_viewController implements Initializable {
+
+    @FXML
+    private TextField campoUsuario;
+
+    @FXML
+    private PasswordField campoContrasena;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Este método se ejecuta automáticamente al cargar la vista
+        System.out.println("Vista cargada correctamente");
+    }
+
+    @FXML
+    private void iniciarSesion() {
+
+        String usuario = campoUsuario.getText();
+        String contrasena = campoContrasena.getText();
+
+        System.out.println("Usuario: " + usuario);
+        System.out.println("Contraseña: " + contrasena);
+
+    }
+
+}
